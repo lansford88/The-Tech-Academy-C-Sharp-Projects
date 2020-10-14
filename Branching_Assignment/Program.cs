d81b906b19837fc7fe6ad50a50911c19ffcee59a
lansford88 <rplansford@gmail.com>
2020-10-14 16:04:23 -0700

    Branching Assignment
    Making use of if statments  with taking weight/height/length and width and producing availability for shipping and cost.

---
+++ Branching_Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Branching_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow instructions below");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter your package WEIGHT");
+            int  packWeight = Convert.ToInt32(Console.ReadLine());
+
+
+            if (packWeight > 50) 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+            }
+            
+
+            Console.WriteLine("Please enter package width:");
+            int widthVar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter package height:");
+            int heightVar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter package length:");
+            int lengthVar = Convert.ToInt32(Console.ReadLine());
+           
+            if (widthVar + heightVar + lengthVar >= 50) 
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express");
+            }
+
+            int costPre = widthVar * heightVar * lengthVar;
+            double totalCost = costPre / 100;
+
+            Console.WriteLine("Your estimated total cost for shipping this package is: $"+ totalCost);
+            Console.ReadLine();
+            Console.WriteLine("Thank you for your business");
+        }
+    }
+}
