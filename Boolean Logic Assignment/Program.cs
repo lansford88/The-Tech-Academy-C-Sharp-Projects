4702a25ddc550e382997164678d54e1dad78d89d
lansford88 <rplansford@gmail.com>
2020-10-13 23:35:21 -0700

    Boolean Logic Assignment

---
+++ Boolean Logic Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int ageVar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Please answer True or False.");
+            bool duiVar = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticketVar = Convert.ToInt32(Console.ReadLine());
+
+            bool qualAnswer = ageVar >= 15 && duiVar == false  && ticketVar <= 3;
+            Console.WriteLine("Qualified?\n"  + qualAnswer);
+            Console.ReadLine();
+
+        }
+    }
+}
