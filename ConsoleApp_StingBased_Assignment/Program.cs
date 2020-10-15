2b112e70013cd135fad302c3accdeaab7090835d
lansford88 <rplansford@gmail.com>
2020-10-14 23:24:00 -0700

    String BasedApplication

---
+++ ConsoleApp_StingBased_Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp_StingBased_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string f_Name = "Ryan";
+            string last_Name = " Patrick";
+            string dreamJob = " To CREATE Unity Assets and work with a development team on a game!";
+
+            Console.WriteLine(f_Name + last_Name + dreamJob);
+            Console.ReadLine();
+
+            Console.WriteLine(f_Name.ToUpper());
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Ryan\n");
+            sb.Append("My last name is Parick\n");
+            sb.Append("My goal for this course is to learn and then build my own projects!");
+            
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
