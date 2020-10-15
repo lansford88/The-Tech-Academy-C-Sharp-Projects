1f2b6f4b520c3557ea30d3c48177be2a489084fb
lansford88 <rplansford@gmail.com>
2020-10-14 21:49:11 -0700

    Updated Branching Assignment

--- Branching_Assignment/Program.cs
+++ Branching_Assignment/Program.cs
@@ -10,35 +10,36 @@ namespace Branching_Assignment
             Console.ReadLine();
 
             Console.WriteLine("Please enter your package WEIGHT");
-            int  packWeight = Convert.ToInt32(Console.ReadLine());
+            int packWeight = Convert.ToInt32(Console.ReadLine());
 
-
-            if (packWeight > 50) 
+            if (packWeight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
             }
+            else if (packWeight <= 50)
             
+                Console.WriteLine("Please enter package width:");
+                int widthVar = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter package width:");
-            int widthVar = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter package height:");
+                int heightVar = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter package height:");
-            int heightVar = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter package length:");
-            int lengthVar = Convert.ToInt32(Console.ReadLine());
-           
-            if (widthVar + heightVar + lengthVar >= 50) 
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express");
-            }
+                Console.WriteLine("Please enter package length:");
+                int lengthVar = Convert.ToInt32(Console.ReadLine());
 
             int costPre = widthVar * heightVar * lengthVar;
             double totalCost = costPre / 100;
 
-            Console.WriteLine("Your estimated total cost for shipping this package is: $"+ totalCost);
-            Console.ReadLine();
-            Console.WriteLine("Thank you for your business");
+            if (widthVar + heightVar + lengthVar >= 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express");
+            }
+            else
+                Console.WriteLine("Your estimated total cost for shipping this package is: $" + totalCost);
+                Console.ReadLine();
+                Console.WriteLine("Thank you for your business");
+                Console.ReadLine();
+            
+            }
         }
     }
-}
