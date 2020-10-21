4a27b27d7dc581f900c1d389989fdb200ba12b7d
lansford88 <rplansford@gmail.com>
2020-10-21 11:40:09 -0700

    Class_Assign 216

---
+++ Class_Assignment_216/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Assignment_216
+{
+    class MyClass
+    {
+        public static int sumNum(int num1)
+        {
+            return num1 + 10;
+        }
+
+        public static int multNum(int num1)
+        {
+            return num1 * 10;
+        }
+
+        public static  int subNum(int num1)
+        { 
+            return num1  - 10;
+        }
+    }
+}
---
+++ Class_Assignment_216/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Class_Assignment_216
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to do the operation on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}  {1}  {2}", MyClass.subNum(userInput), MyClass.sumNum(userInput), MyClass.multNum(userInput));
+            Console.ReadLine();
+        }
+    }
+}
