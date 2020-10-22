24f635a5a48be382773439d021d597f9cacdf7d9
lansford88 <rplansford@gmail.com>
2020-10-22 02:58:36 -0700

    Class_Assignment_Overload

---
+++ Class_Assignment_219/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Assignment_219
+{
+    public class MyClass
+    {
+        public static int Addition(int num1)
+        {
+            return num1 + 50;
+        }
+
+        public static double Addition(double num2)
+        {
+            return num2 *  50.5;
+        }
+        public static string Addition(string num3)
+        {
+            int x = Convert.ToInt32(num3);
+            int y = (x * 2);
+            num3 = y.ToString();
+           return num3;
+        }
+    }
+}
---
+++ Class_Assignment_219/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Class_Assignment_219
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           MyClass myClass = new MyClass();
+
+            int num1 = MyClass.Addition(25);
+            double num2 = MyClass.Addition(1.5);
+            string num3 = MyClass.Addition("10");        
+
+            Console.WriteLine("Int :" + num1);
+            Console.WriteLine("Double :" + num2);
+            Console.WriteLine("String:" + num3);
+            
+            Console.ReadLine();
+         }
+        
+    }
+}
