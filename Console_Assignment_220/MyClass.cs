ec198f6a961fc8851170f82c6b118d04f691248b
lansford88 <rplansford@gmail.com>
2020-10-23 12:31:40 -0700

    Console_220

---
+++ Console_Assignment_220/MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_Assignment_220
+{
+    public class MyClass
+    {
+        public  int Addition(int num1, int num2 = 10)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Console_Assignment_220/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Console_Assignment_220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass a = new MyClass();
+
+            Console.WriteLine("Please enter two number, one at a time");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Enter your second number, or you can leave this blank");
+            var userinput2 = Console.ReadLine();
+
+
+
+            if (userinput2 != "")
+            {
+                int ui2 = Convert.ToInt32(userinput2);
+                Console.WriteLine(a.Addition(userInput1, ui2));
+            }
+            else
+            {
+                Console.WriteLine(a.Addition(userInput1));
+            }
+            Console.ReadLine();
+            
+
+        }
+    }
+}
