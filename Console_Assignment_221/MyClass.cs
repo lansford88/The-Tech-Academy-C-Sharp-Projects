d50c4def2efcfb722b77d11c12834ff4743ab440
lansford88 <rplansford@gmail.com>
2020-10-23 21:55:33 -0700

    Console_Assignment 221

---
+++ Console_Assignment_221/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_Assignment_221
+{
+    class MyClass
+    {
+        public void Addition(int x, int y)
+        {   
+            int num1 = x * 5;
+            Console.WriteLine(num1);
+            Console.WriteLine(y);
+            
+        }
+        
+    }
+}
---
+++ Console_Assignment_221/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Console_Assignment_221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass a = new MyClass();
+
+
+
+            //passing two numbers
+            a.Addition(50, 50);
+            Console.ReadLine();
+
+            //specify by name
+            a.Addition(x:75, y:10);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
