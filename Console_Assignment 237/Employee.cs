24b7513285d39471ad880de91578790322788f83
lansford88 <rplansford@gmail.com>
2020-10-24 03:45:57 -0700

    Abstraction Assignment

---
+++ Console_Assignment 237/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_Assignment_237
+{
+    class Employee  : Person
+    {
+        public override void SayName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Console_Assignment 237/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Console_Assignment_237
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(); // Instantiate Employee Object
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            employee.SayName();
+        }
+    }
+}
