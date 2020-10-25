b120d908ad0a42b6df9935ab4650698384476ed6
lansford88 <rplansford@gmail.com>
2020-10-25 03:53:35 -0700

    Struct_Practice_Assignment

---
+++ Console_Assignment_Struct_260/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Console_Assignment_Struct_260
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Number fullCost = new Number();
+            fullCost.Amount =140.00m;
+
+            Console.WriteLine(fullCost.Amount);
+            Console.ReadLine();
+        }
+
+         struct Number
+        { 
+        
+        
+            public decimal Amount { get; set; }
+        }
+    }
+}
