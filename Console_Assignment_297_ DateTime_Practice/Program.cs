eba3d6b581f438f1996ca10906afab94b65e1b6e
lansford88 <rplansford@gmail.com>
2020-10-25 23:15:12 -0700

    Console_Assignment_297_DateTime Practice
    This assignment has us print the current date and time, then ask the user for a number. Once we have the user input, we use that information to add to the hours to the current time.

---
+++ Console_Assignment_297_ DateTime_Practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Console_Assignment_297__DateTime_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //This will give you the time that is NOW
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+
+            //This asks the user to input a number and then assigns that number to the variable userInput
+            Console.WriteLine("\nPlease choose a number : ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+             Console.ReadLine();
+            
+            //DateTime.Now gives you the time now, adding .AddHours(built in) will let you add actual hours to DateTime.Now. in the parenthesis it takes the integer to add.
+            Console.WriteLine(DateTime.Now.AddHours(userInput));
+            Console.ReadLine();
+        }
+    }
+}
