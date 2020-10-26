d91199e1d4a075bae8229a5a58d01ecee7828512
lansford88 <rplansford@gmail.com>
2020-10-25 21:23:00 -0700

    Console_Assignment_290_Log Practice
    This assignment makes use of the System.IO and the writeing to  a .txt file and then the printing from the .txt file.

---
+++ Console_Assignment_294_Log_Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Console_Assignment_294_Log_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This will ask the user for a number and then will print that answer (userinput) to the .txt ile in the path.
+            Console.WriteLine("Please enter a number");
+            String userInput = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Ryan\Desktop\Basic_C#_Programs\log\log.txt", userInput);
+
+            // This next section will take the contents of that .txt file and then print them back to the user.
+            //string userInput = File.ReadAllText(@"C:\Users\Ryan\Desktop\Basic_C#_Programs\log\log.txt");
+            //Console.WriteLine(userInput);
+            //Console.ReadLine();
+        }
+    }
+}
