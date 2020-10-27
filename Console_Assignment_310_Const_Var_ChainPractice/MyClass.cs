5e462386953cff9ea5f070673f549cb43a21b343
lansford88 <rplansford@gmail.com>
2020-10-27 00:24:07 -0700

    Console_Assignment_310_Var+Const_ConstChain

---
+++ Console_Assignment_310_Const_Var_ChainPractice/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Console_Assignment_310_Const_Var_ChainPractice
+{
+    class MotorCycle
+    {
+        public string model;
+        public string color;
+
+
+        public MotorCycle(string modelName) : this(modelName, "Red")
+        {
+        }
+
+        public MotorCycle(string modelName, string modelcolor)
+        {
+            color = modelcolor;
+            model = modelName;
+        }
+
+
+    }
+}
---
+++ Console_Assignment_310_Const_Var_ChainPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Console_Assignment_310_Const_Var_ChainPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var variable
+            var newRideA = "CB";
+
+            //const variable
+            const string myCatchPhrase = "Gunna be dumb, gotta be tough";
+
+
+            //Use of Constructor Chain.
+            MotorCycle newBike = new MotorCycle("CB750K");
+            newBike.model = "Honda";
+
+            Console.WriteLine("I just got a new {0} {1} ", newBike.color,newBike.model);
+            Console.ReadLine();
+        }
+    }
+}
