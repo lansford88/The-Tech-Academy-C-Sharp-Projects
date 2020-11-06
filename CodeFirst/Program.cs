aa0a102628225e139f0fac2dcf0ae770f00f715a
lansford88 <rplansford@gmail.com>
2020-11-06 00:44:02 -0800

    CodeFirst_Example

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Jason" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+
+    }
+}
